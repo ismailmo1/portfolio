25eef78260e1952e996829c1effa20f43ad862a4
Ismail Mohammed <ismailmo4@gmail.com>
2021-11-13 00:26:16 +0000

    industrial bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
-const IndexPage= () =>{
-  return(
+const IndexPage = () => {
+  return (
     <Layout pageTitle="Home Page">
       <p>Checkout some of the cool projects i've built!</p>
-    <StaticImage alt= "manufacturing and data" src="../images/chemdata.jpeg"/>
-          </Layout>
+      <StaticImage objectFit="cover" objectPosition="absolute" src="../images/homepage-bg.jpg" />
+    </Layout>
   )
 }
 
